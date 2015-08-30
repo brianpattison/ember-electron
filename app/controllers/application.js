d614f65b4f27b5fc9d9d0db088489911ae286ae6
Brian Pattison <brian@brianpattison.com>
2015-08-30 00:40:11 -0500

    Add settings menu

---
+++ app/controllers/application.js
@@ -0,0 +1,33 @@
+import Ember from 'ember';
+var remote = requireNode('remote');
+var Menu = remote.require('menu');
+var MenuItem = remote.require('menu-item');
+
+var menu = new Menu();
+menu.append(new MenuItem({
+  label: 'Menu item #1',
+  click: function() {
+    console.log('Menu item #1');
+  }
+}));
+menu.append(new MenuItem({
+  label: 'Menu item #2',
+  click: function() {
+    console.log('Menu item #2');
+  }
+}));
+menu.append(new MenuItem({ type: 'separator' }));
+menu.append(new MenuItem({
+  label: 'Menu item #3',
+  click: function() {
+    console.log('Menu item #3');
+  }
+}));
+
+export default Ember.Controller.extend({
+  actions: {
+    showSettingsMenu: function() {
+      menu.popup(remote.getCurrentWindow());
+    }
+  }
+});
