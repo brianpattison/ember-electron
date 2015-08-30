abaf9bddfabf8e2145e965587f9532ce66f4ff56
Brian Pattison <brian@brianpattison.com>
2015-08-30 02:53:21 -0500

    Set body css class based on current route

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -29,5 +29,17 @@ export default Ember.Controller.extend({
     showSettingsMenu: function() {
       menu.popup(remote.getCurrentWindow());
     }
-  }
+  },
+
+  currentPathDidChange: function() {
+    var currentPath = this.get('currentPath');
+
+    // Set the body class based on route
+    var splitPath = currentPath.split('.').map(function(path) {
+      return Ember.String.dasherize(path);
+    });
+
+    var bodyClass = 'ember-application ' + splitPath.join(' ');
+    $('body').attr('class', bodyClass);
+  }.observes('currentPath'),
 });
